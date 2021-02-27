172cf16b19c0f581fb5ff5d0e666393a942c2e52
prashant-humney <prashant.humney89@gmail.com>
2021-02-27 16:47:47 +0530

    Created Collection View Programmatically and used UIWindow to show ViewController.swift as root VC

--- FacebookFeed/ViewController.swift
+++ FacebookFeed/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  let collectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+    let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    return view
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    setupCollectionView()
   }
 
-
+  private func setupCollectionView() {
+    collectionView.backgroundColor = UIColor.red
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(collectionView)
+    
+    NSLayoutConstraint.activate([
+      view.leadingAnchor.constraint(equalTo: collectionView.leadingAnchor),
+      view.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor),
+      view.topAnchor.constraint(equalTo: collectionView.topAnchor),
+      view.bottomAnchor.constraint(equalTo: collectionView.bottomAnchor)
+    ])
+  }
 }
 
